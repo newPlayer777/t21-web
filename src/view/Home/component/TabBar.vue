06814c2c1055a7a9edbe9758a9c908e8c5de4ece
player777 <962132784@qq.com>
2025-07-01 11:50:25 +0800

    feat: 1.添加tab样式 2.安装less

--- src/view/Home/component/TabBar.vue
+++ src/view/Home/component/TabBar.vue
@@ -1,7 +1,23 @@
 <template>
-    <div>我是tabBar</div>
+    <div class="flex flex-row justify-between items-center bg-white shadow-sm p-[24px]">
+        <div class="flex flex-row justify-center items-center">
+            <h1 class="text-xl font-bold text-center">T21</h1>
+            <img class="size-[40px] ml-2" src="/src/assets/logo.png" />
+        </div>
+        <div class="flex flex-row items-center space-x-4">
+            <div class="text-sm text-[#333]" v-for="(item, index) in tabList" :key="index">
+                {{ item.name }}
+            </div>
+        </div>
+    </div>
 </template>
+
 <script lang="ts" setup>
+const tabList: Array<{ name: string }> = [
+    { name: '首页' },
+    { name: '帮助中心' },
+    { name: '关于我们' },
+]
 </script>
 
 <style module="TabBar" lang="less"></style>
